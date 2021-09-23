0985d3ea3972a0b445ca55378fc5ba5b2e137101
Andres Sanchez <anfsanchezcu@unal.edu.co>
2021-09-23 15:20:25 -0500

    funcionalidad 40%

--- js/main.js
+++ js/main.js
@@ -1,12 +1,51 @@
-let btnToggle =document.getElementById("menu-toggle-btn");
- btnToggle.addEventListener("click",()=>activarSideBar());
-let btnClose =document.getElementById("close-menu");
-btnClose.addEventListener("click",()=>activarSideBar());
+//variables
+let ruta;
+let aux;
 
 
 
+//-------------------Activadores---------------
+//menu
+document.getElementById("menu-toggle-btn").addEventListener("click",()=>activarSideBar());
+document.getElementById("close-menu").addEventListener("click",()=>{
+    activarSideBar();
+    ocultarMenu();
+});
+document.getElementsByClassName("close-sub-menu")[0].addEventListener("click",()=>ocultarMenu());
+
+//sub-menu
+let ul = document.getElementById("menu-0").getElementsByTagName("li");
+for(i=1; i<ul.length; i++) {
+    ruta="-0-1";
+   ul[i].addEventListener("click",()=>mostrarMenu(ruta))
+}
+
+
+
+
+
+
+//Funciones
 function activarSideBar(){
-    console.log("hola")
     document.getElementById("side-bar").classList.toggle("active");
     document.getElementsByClassName("menu-container")[0].classList.toggle("active");
+    let ul_menu=document.getElementById("menu-0");
+    if(ul_menu.classList.toggle("hidden-left")){
+        ul_menu.classList.toggle("hidden-left");
+        console.log("si la tiene")
+    }
+}
+function mostrarMenu(e){
+    aux= "menu"+e;
+    document.getElementById("menu-0").classList.toggle("hidden-left");
+    document.getElementById(aux).classList.toggle("menuActive");
+    
+}
+function ocultarMenu(e){
+    document.getElementById("menu-0").classList.toggle("hidden-left");
+    let to_show = document.getElementById(aux)
+    if(to_show!=null){
+        to_show.classList.toggle("menuActive")
+        console.log("entro")
+    }
 }
