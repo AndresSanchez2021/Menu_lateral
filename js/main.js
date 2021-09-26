58c9d2c7be7f39215537a02ffdda31cf42309676
Andres Sanchez <anfsanchezcu@unal.edu.co>
2021-09-25 23:57:38 -0500

    optimisazion de logica

--- js/main.js
+++ js/main.js
@@ -1,61 +1,56 @@
 //variables
-let ruta="menu-pr";
+const origen="menu-pr";
+let actual=origen;
+
+const mantenimiento ="menu-pr-#";
 
 
 
 //-------------------Activadores---------------
 //menu
-document.getElementById("menu-toggle-btn").addEventListener("click",()=>activarSideBar());
-document.getElementById("close-menu").addEventListener("click",()=>{
-    ocultarMenu();
-    activarSideBar();
-});
-let close_arrow=document.getElementsByClassName("close-sub-menu");
-    for(let i=0; i<close_arrow.length; i++){
-        close_arrow[i].addEventListener("click",ocultarMenu);
-    }
+document.getElementById("menu-toggle-btn").addEventListener("click",()=>activarSideBar(true));
+document.getElementById("close-menu").addEventListener("click",()=>activarSideBar(false));
+
 
 //sub-menu
 let ul = document.getElementsByClassName("item");
 for (let i =0; i<ul.length; i++) {
-    ul[i].addEventListener("click",()=>mostrarMenu(ruta+"-"+ul[i].value));
+    ul[i].addEventListener("click",()=>mostrarMenu(actual+"-"+ul[i].value));
 }
+let close_arrow=document.getElementsByClassName("close-sub-menu");
+    for(let i=0; i<close_arrow.length; i++){
+        close_arrow[i].addEventListener("click",()=>mostrarMenu(origen));
+    }
 
    
 
 
-
-
-
-
 //Funciones
-function activarSideBar(){
-    ruta="menu-pr"
+function activarSideBar(p){
     document.getElementById("side-bar").classList.toggle("active");
     document.getElementsByClassName("side-bar-container")[0].classList.toggle("active");
-    document.getElementsByClassName("menu-pr")[0].classList.toggle("active");
-    
-}
-
-function mostrarMenu(e){
-    ruta=e;
-    console.log(ruta);
-    if(ruta.substring(0, ruta.length-2)=="menu-pr")
-        document.getElementById("menu-pr").classList.toggle("active"); 
-    
-    if(ruta.length>9)
-        ruta=ruta.substring(0, ruta.length-2)
-    else
-        document.getElementById(ruta).classList.toggle("active");
-    
-    
-}
-function ocultarMenu(){
-    console.log(ruta.length)
-    if(ruta.length>7){
-        document.getElementById(ruta).classList.toggle("active");
-        ruta=ruta.substring(0, ruta.length-2)
-        console.log(ruta);
-        document.getElementById(ruta).classList.toggle("active");
+    if(p)
+        document.getElementById(actual).classList.toggle("active"); 
+    else{
+        ocultarMenu();
     }
 }
+function mostrarMenu(dir){
+    let aux =document.getElementById(dir);
+    if(aux!=null && dir!=actual){
+        document.getElementById(actual).classList.toggle("active"); 
+        aux.classList.toggle("active"); 
+        actual=dir;
+    }else{
+        mostrarMenu(mantenimiento);
+    }
+}
+function ocultarMenu(){
+    document.getElementById(actual).classList.toggle("active");
+    actual=origen;
+}
+
+
+
+
+
